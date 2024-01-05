5b3fb39d0daffe0f2347c0ed399a6ea9172b70e8
Denis Untevskiy <theoden-dd@users.noreply.github.com>
2024-01-05 19:00:26 +0200

    On button click plot only the selected nodes

--- graph_conditional/_tcltk.py
+++ graph_conditional/_tcltk.py
@@ -1,6 +1,8 @@
 """Internal module with tcl/tk tools."""
 import tkinter as tk
 
+from graph_conditional.graphviz import plot_graph
+
 
 def run_tkinter(tree):
     window = tk.Tk()
@@ -9,6 +11,12 @@ def run_tkinter(tree):
 
     tree_controls = {}
     _build_tree_controls(tree, window, tree_controls)
+
+    def plot_graph_click():
+        plot_graph(tree, tree_controls)
+
+    b = tk.Button(window, text='Вывести граф', command=plot_graph_click)
+    b.pack(side=tk.BOTTOM)
 
     window.mainloop()
 
