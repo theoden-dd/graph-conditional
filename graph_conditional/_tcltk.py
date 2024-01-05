a4a3b61f5b388d6d1ade1446550af256f49dc8f2
Amani Mena <1140785-the0den@users.noreply.gitlab.com>
2024-01-05 18:20:09 +0200

    View a tree of checkboxes respecting the input tree

--- graph_conditional/_tcltk.py
+++ graph_conditional/_tcltk.py
@@ -2,14 +2,31 @@
 import tkinter as tk
 
 
-def run_tkinter():
+def run_tkinter(tree):
     window = tk.Tk()
     window.title('Условный граф')
     window.geometry('640x480')
 
-    var1 = tk.IntVar()
-    c1 = tk.Checkbutton(window, text='Python', variable=var1, onvalue=1, offvalue=0)
-    c1.pack()
+    tree_controls = {}
+    _build_tree_controls(tree, window, tree_controls)
 
     window.mainloop()
 
+
+def _build_tree_controls(node: dict, control_window: tk.Tk, controls: dict, level=0) -> None:
+    node_name = node['name']
+
+    control_var = tk.IntVar(value=1)
+    control = tk.Checkbutton(
+        control_window, text=node['label'], variable=control_var, onvalue=1, offvalue=0, anchor='w',
+    )
+    control.pack(padx=level*5, fill=tk.X)
+
+    controls[node_name] = {
+        'control': control,
+        'var': control_var
+    }
+
+    if 'children' in node:
+        for child in node['children']:
+            _build_tree_controls(child, control_window, controls, level+1)
