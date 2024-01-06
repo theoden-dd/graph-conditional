046174fa99f18287f740e6d19633fc9b1e0bb0fa
Denis Untevskiy <theoden-dd@users.noreply.github.com>
2024-01-06 17:08:03 +0200

    Add a selection dialog for tree configuration
    After the selection a modal with checkboxes for the chosen configuration is opened.

--- graph_conditional/main.py
+++ graph_conditional/main.py
@@ -1,14 +1,6 @@
-from pathlib import Path
-
-import tomli
 
 from graph_conditional._tcltk import run_tkinter
 
 
 if __name__ == '__main__':
-    home = Path.home()
-
-    with open(home / '.graph-conditional.toml', 'rb') as f:
-        config = tomli.load(f)
-
-    run_tkinter(config)
+    run_tkinter()
